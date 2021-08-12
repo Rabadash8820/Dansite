8bfa96bdabb2f16ddddc912bdb751599a3c6c545
Dan Vicarel <dvicarel@summitracing.com>
2021-08-12 18:13:47 -0400

    WIP: Displaying skills as badges

--- src/about/data/skillGroups.ignored.js
+++ src/about/data/skillGroups.ignored.js
@@ -1,16 +1,38 @@
 export default {
-    general: {
+    operatingSystems: {
         heading: {
-            id: "skills-general",
+            id: "skills-operatingSystems",
             level: 3
         },
-        skills: []
     },
-    technology: {
+    languages: {
         heading: {
-            id: "skills-technology",
+            id: "skills-languages",
             level: 3
         },
-        skills: []
-    }
+    },
+    databases: {
+        heading: {
+            id: "skills-databases",
+            level: 3
+        },
+    },
+    frameworksLibs: {
+        heading: {
+            id: "skills-frameworksLibs",
+            level: 3
+        },
+    },
+    tools: {
+        heading: {
+            id: "skills-tools",
+            level: 3
+        },
+    },
+    misc: {
+        heading: {
+            id: "skills-misc",
+            level: 3
+        },
+    },
 }
--- src/about/i18n/en-US/skillGroups.en-US.ignored.js
+++ src/about/i18n/en-US/skillGroups.en-US.ignored.js
@@ -1,30 +1,90 @@
 const HEADING_ARIA_LBL = "Anchor"
 
 export default {
-    general: {
+    operatingSystems: {
         heading: {
-            text: "General",
+            text: "Operating Systems",
         },
         skills: [
-            { skill: "Very quick to learn new tasks and adapt to new work environments" },
-            { skill: "Experience providing courteous and professional customer service" },
-            { skill: "Excellent technical writing, presentation, and public speaking skills" },
-            { skill: "Intermediate German, able to write and speak" },
+            { skill: "Windows (XP / Vista / 7 / 8 / 10)" },
+            { skill: "Linux (Ubuntu, Amazon Linux/RHEL)" },
         ]
     },
-    technology: {
+    languages: {
         heading: {
-            text: "Technology",
+            text: "Languages",
         },
         skills: [
-            { skill: "Very skilled with Microsoft Office Suite (Word, Excel, PowerPoint, Outlook)" },
-            { skill: "Operating Systems: Windows (XP / Vista / 7 / 8 / 10) and Linux (Ubuntu, Amazon Linux/RHEL)" },
-            { skill: "Languages: C#, C++, HTML/CSS, JavaScript (including ES6+), Excel VBA, VB.NET, TI-Basic, MS-DOS and Linux scripting, XML, JSON, YAML, Markdown" },
-            { skill: "Databases: MySQL, SQLite, SQL Server, Redis" },
-            { skill: "Frameworks/libraries: .NET Framework and .NET Core, EntityFramework Core, Xamarin, ASP.NET Core, jQuery, Unity3D, NUnit, Moq, Node.js, Alexa Skills Kit" },
-            { skill: "Tools: Visual Studio (Code), Vim, Notepad++, Git, GitHub, GitKraken, SourceTree, Azure DevOps, ESLint, Babel, EditorConfig, NuGet, Docker, Slack, Postman, Windows Subsystem for Linux, bash, sed, SQL Server Management Studio, Remote Desktop" },
+            { skill: "C#" },
+            { skill: "C++" },
+            { skill: "HTML/CSS" },
+            { skill: "JavaScript (including ES6+)" },
+            { skill: "Excel VBA" },
+            { skill: "VB.NET" },
+            { skill: "TI-Basic" },
+            { skill: "Windows Command Prompt and Linux shell scripting" },
+            { skill: "XML, JSON, YAML" },
+            { skill: "Markdown, Mustache" },
+        ]
+    },
+    databases: {
+        heading: {
+            text: "Databases and storage tools",
+        },
+        skills: [
+            { skill: "Relational DBMSs: MySQL, SQLite, SQL Server, PostgreSQL" },
+            { skill: "NoSQL DBMSs: Redis, DynamoDB" },
+            { skill: "NoSQL DBMSs: Redis, DynamoDB" },
+            { skill: "Apache Kafka and Avro" },
+            { skill: "GUIs: SQL Server Management Studio, MySQL Workbench, DB Browser for SQLite, Azure Data Studio" },
+        ]
+    },
+    frameworksLibs: {
+        heading: {
+            text: "Frameworks/libraries",
+        },
+        skills: [
+            { skill: ".NET Framework and .NET Core" },
+            { skill: "EntityFramework Core" },
+            { skill: "Xamarin (primarily Xamarin.Android)" },
+            { skill: "ASP.NET Core" },
+            { skill: "jQuery" },
+            { skill: "Unity3D" },
+            { skill: "NUnit and Moq" },
+            { skill: "Node.js" },
+            { skill: "Alexa Skills Kit" },
+        ]
+    },
+    tools: {
+        heading: {
+            text: "Tools",
+        },
+        skills: [
+            { skill: "Visual Studio (Code)" },
+            { skill: "Vim (including, but not limited to), how to close it :P" },
+            { skill: "Notepad++" },
+            { skill: "Git and SVN" },
+            { skill: "Git clients: GitKraken, SourceTree, Git GUI, gource" },
+            { skill: "Git hosts: GitHub, Azure DevOps, BitBucket" },
+            { skill: "Node.js build tools : ESLint, Babel, Uglify.js, Prettier.js, html-minifier, PostCSS" },
+            { skill: "EditorConfig" },
+            { skill: "NuGet" },
+            { skill: "Docker" },
+            { skill: "Communication tools: Slack, Discord, Skype" },
+            { skill: "Postman" },
+            { skill: "Windows Subsystem for Linux" },
+            { skill: "Shells: bash, command prompt, sed" },
+            { skill: "Remote Desktop" },
+        ]
+    },
+    misc: {
+        heading: {
+            text: "Miscellaneous",
+        },
+        skills: [
+            { skill: "Microsoft Office Suite (Word, Excel, PowerPoint, Access, Outlook, OneDrive, OneNote)" },
             { skill: "Considerable experience with computer hardware, networking, and the graphics pipeline" },
             { skill: "Considerable experience with cloud and DevSecOps practices using Amazon Web Services" },
         ]
-    }
+    },
 }
