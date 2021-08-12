0704559b34db75585ce7f77050dfdd66978f325c
Dan Vicarel <shundra8820@gmail.com>
2021-08-12 18:01:14 -0400

    Updated URLs in secrets.template.js, now that its been a year and a half...

--- src/build/secrets.template.js
+++ src/build/secrets.template.js
@@ -1,10 +1,10 @@
 export default {
     emailjs: {
-        userId: "user_AAAAAAAAAAAAAAAAAAAAA",   // Find at https://dashboard.emailjs.com/account > API KEYS
-        serviceId: "service-id",                // Look for correct Service ID at https://dashboard.emailjs.com
-        templateId: "template_id",              // Look for correct Template ID at https://dashboard.emailjs.com/templates
+        userId: "user_AAAAAAAAAAAAAAAAAAAAA",   // Find at https://dashboard.emailjs.com/admin/integration > API KEYS
+        serviceId: "service-id",                // Look for correct Service ID at https://dashboard.emailjs.com/admin
+        templateId: "template-id",              // Look for correct Template ID at https://dashboard.emailjs.com/admin/templates
     },
     recaptcha: {
-        siteKey: "AAAAAAAAAAAAAAAAAAAAAAAAAA_AAAAAAAAAAAAA"     // Find at https://www.google.com/recaptcha/admin > Settings > reCAPTCHA keys
+        siteKey: "AAAAAAAAAAAAAAAAAAAAAAAAAA_AAAAAAAAAAAAA"     // Find at https://www.google.com/recaptcha/admin, pick the right site, then check Settings > reCAPTCHA keys
     }
 }
