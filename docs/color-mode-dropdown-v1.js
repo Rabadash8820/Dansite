d23d4aaa46219d89c8c6f508d171c508d776670a
Dan Vicarel <shundra8820@gmail.com>
2023-09-06 05:05:49 +0000

    Add a color mode dropdown! - Basically copied from the dropdown script on the Bootstrap Docs website - Color mode defaults to auto (match OS) - Header gradient colors also now use Bootstrap CSS variables

---
+++ docs/color-mode-dropdown-v1.js
@@ -0,0 +1,72 @@
+/*!
+ * Adapted from the Color mode toggler for Bootstrap's docs (see https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+  'use strict'
+
+  function getStoredTheme() { localStorage.getItem('theme') }
+  function setStoredTheme(theme) { localStorage.setItem('theme', theme) }
+
+  function getPreferredTheme() {
+    const storedTheme = getStoredTheme()
+    return storedTheme
+      ? storedTheme
+      : window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+
+  function setTheme(theme) {
+    const bsTheme = theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : theme
+    document.documentElement.setAttribute('data-bs-theme', bsTheme)
+  }
+
+  setTheme(getPreferredTheme())
+
+  function showActiveTheme(theme, focus = false) {
+    const btnThemeSwitcher = document.getElementById('dan-theme')
+    if (!btnThemeSwitcher)
+      return
+
+    const lblThemeSwitcher = document.getElementById('dan-theme-text')
+    const activeThemeIconSvg = btnThemeSwitcher.getElementsByTagName('svg')[0]
+    const btnToActivate = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+    const btnToActivateSvg = btnToActivate.getElementsByClassName('theme-icon')[0]
+
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+      element.classList.remove('active')
+      element.setAttribute('aria-pressed', 'false')
+    })
+
+    btnToActivate.classList.add('active')
+    btnToActivate.setAttribute('aria-pressed', 'true')
+    activeThemeIconSvg.replaceWith(btnToActivateSvg.cloneNode(true))
+    btnThemeSwitcher.setAttribute('aria-label', `${lblThemeSwitcher.textContent} (${btnToActivate.dataset.bsThemeValue})`)
+
+    if (focus)
+      btnThemeSwitcher.focus()
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme !== 'light' && storedTheme !== 'dark')
+      setTheme(getPreferredTheme())
+  })
+
+  window.addEventListener('DOMContentLoaded', () => {
+    showActiveTheme(getPreferredTheme())
+
+    document.querySelectorAll('[data-bs-theme-value]')
+      .forEach(toggle => {
+        toggle.addEventListener('click', () => {
+          const theme = toggle.getAttribute('data-bs-theme-value')
+          setStoredTheme(theme)
+          setTheme(theme)
+          showActiveTheme(theme, true)
+        })
+      })
+  })
+})()
